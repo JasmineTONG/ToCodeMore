4a4faad33cf3cffce767a5d249f5f8e4c3f02141
Jiawen Tong <tongjw1@outlook.com>
2017-08-29 16:32:42 -0400

    modified comment

--- maxDepth-clean.py
+++ maxDepth-clean.py
@@ -1,8 +1,8 @@
 # https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
-# Definition for a binary tree node.
 # Given a binary tree, find its maximum depth. 
 # The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
 
+# Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
 #         self.val = x
